b15f9a3f3f26270c2590435df907353fc34ce9c6
Venkat Bommineni <bomminenivenkat@gmail.com>
2025-07-28 11:53:12 +0000

    Updated app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from EKS! Deployed across multiple regions using GitHub Actions and AWS!"
+    return "Hello from EKS! VENKAT- Deployed across multiple regions using GitHub Actions and AWS!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
